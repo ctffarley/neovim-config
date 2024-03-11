ffd17c9991a4bebec9c5edd15696b6d76472dc40
Clark Farley <clarktfarley@gmail.com>
2024-03-11 00:16:15 -0400

    First additions after kickstart
    - Change to onedark
    
    - Add the vim options and keybinds I like
    
    - Add vim-stay

--- init.lua
+++ init.lua
@@ -91,18 +91,12 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
 -- NOTE: You can change these options as you wish!
 --  For more options, you can see `:help option-list`
-
--- Make line numbers default
-vim.opt.number = true
--- You can also add relative line numbers, for help with jumping.
---  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
@@ -149,7 +143,7 @@ vim.opt.inccommand = 'split'
 vim.opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 10
+vim.opt.scrolloff = 15
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
@@ -232,9 +226,11 @@ require('lazy').setup({
   -- Use `opts = {}` to force a plugin to be loaded.
   --
   --  This is equivalent to:
-  --    require('Comment').setup({})
+  -- require('Comment').setup({})
 
   -- "gc" to comment visual regions/lines
+  -- "gb" to comment blocks
+  -- "gcc" to comment in normal mode
   { 'numToStr/Comment.nvim', opts = {} },
 
   -- Here is a more advanced example where we pass configuration
@@ -533,7 +529,7 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         -- clangd = {},
-        -- gopls = {},
+        gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
@@ -817,7 +813,7 @@ require('lazy').setup({
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
 }, {
   ui = {
     -- If you have a Nerd Font, set icons to an empty table which will use the
@@ -839,6 +835,8 @@ require('lazy').setup({
     },
   },
 })
+
+require 'vim-options'
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
---
+++ lua/vim-options.lua
@@ -0,0 +1,16 @@
+vim.cmd 'syntax on'
+
+vim.opt.expandtab = true
+vim.opt.termguicolors = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+-- turn on line numbers and relative line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+-- force utf-8
+vim.opt.encoding = 'utf-8'
+
+vim.g.mapleader = ' '
+
+-- vim.keymap.set('n', '<leader>nh', ':nohl<CR>', {})
