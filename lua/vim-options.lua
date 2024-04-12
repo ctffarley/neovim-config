bd4b5974d89be98fdf2417f9ccf4f5119baa344d
Clark Farley <clarktfarley@gmail.com>
2024-04-12 17:26:16 -0400

    remove duplicate/commented options

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,5 +1,3 @@
--- vim.cmd 'syntax on'
-
 vim.opt.expandtab = true
 vim.opt.termguicolors = true
 vim.opt.tabstop = 2
@@ -11,12 +9,8 @@ vim.opt.relativenumber = true
 -- force utf-8
 vim.opt.encoding = 'utf-8'
 
-vim.g.mapleader = ' '
-
 vim.opt.undofile = true
 vim.opt.undodir = os.getenv 'HOME' .. '/.vim/undodir'
-
--- vim.keymap.set('n', '<leader>nh', ':nohl<CR>', {})
 
 vim.cmd [[silent! autocmd! filetypedetect BufRead,BufNewFile *.tf]]
 vim.cmd [[autocmd BufRead,BufNewFile *.hcl set filetype=hcl]]
