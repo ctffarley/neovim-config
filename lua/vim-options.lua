c5824e5c35995a762a788f490a05967a05977691
Clark Farley <clark.farley@stardog.com>
2024-05-09 09:56:12 -0400

    improve line wrapping

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -24,3 +24,6 @@ vim.cmd [[autocmd BufRead,BufNewFile *.hcl set filetype=hcl]]
 vim.cmd [[autocmd BufRead,BufNewFile .terraformrc,terraform.rc set filetype=hcl]]
 vim.cmd [[autocmd BufRead,BufNewFile *.tf,*.tfvars set filetype=terraform]]
 vim.cmd [[autocmd BufRead,BufNewFile *.tfstate,*.tfstate.backup set filetype=json]]
+
+-- Lines don't wrap mid-word
+vim.o.linebreak = true
