d86f7db98b9e2c13327f0c4daefeb21bb402963c
Clark Farley <clarktfarley@gmail.com>
2024-05-09 22:47:29 -0400

    default dark theme and switch toggle order

--- lua/custom/plugins/onedark.lua
+++ lua/custom/plugins/onedark.lua
@@ -2,8 +2,9 @@ return {
   'gnoLLex/onedark.nvim',
   config = function()
     require('onedark').setup {
+      style = 'dark',
       toggle_style_key = '<leader>ts', -- keybind to toggle theme style.
-      toggle_style_list = { 'dark', 'light' },
+      toggle_style_list = { 'light', 'dark' },
     }
     require('onedark').load()
   end,
