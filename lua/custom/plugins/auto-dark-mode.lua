7847fe1380bc71012d566e5563f892609c701c2a
Clark Farley <clarktfarley@gmail.com>
2024-11-24 13:45:58 -0500

    switch to flow theme

--- lua/custom/plugins/auto-dark-mode.lua
+++ lua/custom/plugins/auto-dark-mode.lua
@@ -4,17 +4,27 @@ return {
     update_interval = 1000,
     set_dark_mode = function()
       vim.o.background = 'dark'
-      require('onedark').setup {
-        style = 'dark',
+      require('flow').setup {
+        dark_theme = true,
+        transparent = false,
+        high_contrast = true,
+        fluo_color = 'pink',
+        mode = 'bright',
+        aggressive_spell = false,
       }
-      vim.cmd 'colorscheme onedark'
+      vim.cmd 'colorscheme flow'
     end,
     set_light_mode = function()
       vim.o.background = 'light'
-      require('onedark').setup {
-        style = 'light',
+      require('flow').setup {
+        dark_theme = false,
+        transparent = false,
+        high_contrast = false,
+        fluo_color = 'pink',
+        mode = 'base',
+        aggressive_spell = false,
       }
-      vim.cmd 'colorscheme onedark'
+      vim.cmd 'colorscheme flow'
     end,
     fallback = 'dark',
   },
