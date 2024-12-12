fab15c4bf7d94a2e8fafe106d1b250cda9e76ee4
Clark Farley <clarktfarley@gmail.com>
2024-12-11 21:43:43 -0500

    Modify lualine

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -1,5 +1,41 @@
 return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
-  opts = {},
+  opts = {
+    options = {
+      component_separators = '',
+      section_separators = { left = '', right = '' },
+    },
+    sections = {
+      lualine_a = {
+        {
+          'mode',
+          fmt = function(str)
+            return str:sub(1, 1)
+          end,
+          separator = { left = '' },
+          right_padding = 2,
+        },
+      },
+      lualine_b = { 'filename', 'branch' },
+      lualine_c = {
+        '%=', --[[ add your center compoentnts here in place of this comment ]]
+      },
+      lualine_x = {},
+      lualine_y = { 'filetype', 'progress' },
+      lualine_z = {
+        { 'location', separator = { right = '' }, left_padding = 2 },
+      },
+    },
+    inactive_sections = {
+      lualine_a = { 'filename' },
+      lualine_b = {},
+      lualine_c = {},
+      lualine_x = {},
+      lualine_y = {},
+      lualine_z = { 'location' },
+    },
+    tabline = {},
+    extensions = {},
+  },
 }
