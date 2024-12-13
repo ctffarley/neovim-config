26c6ce3df866e888f0ad23686396dd176b65ad26
Clark Farley <clarktfarley@gmail.com>
2024-12-13 10:38:54 -0500

    Transparent lualine

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -1,41 +1,53 @@
 return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
-  opts = {
-    options = {
-      component_separators = '',
-      section_separators = { left = '', right = '' },
-    },
-    sections = {
-      lualine_a = {
-        {
-          'mode',
-          fmt = function(str)
-            return str:sub(1, 1)
-          end,
-          separator = { left = '' },
-          right_padding = 2,
+  lazy = false,
+  config = function()
+    require('lualine').setup {
+      options = {
+        component_separators = '',
+        section_separators = { left = '', right = '' },
+      },
+      sections = {
+        lualine_a = {
+          {
+            'mode',
+            fmt = function(str)
+              return str:sub(1, 1)
+            end,
+            separator = { left = '' },
+            padding = { left = 0, right = 1 },
+          },
+        },
+        lualine_b = { 'filename', 'branch' },
+        lualine_c = { { '%=', color = 'Normal' } },
+        lualine_x = { { '%=', color = 'Normal' } },
+        lualine_y = { { 'filetype', 'progress' } },
+        lualine_z = {
+          { 'location', separator = { right = '' }, padding = 0 },
         },
       },
-      lualine_b = { 'filename', 'branch' },
-      lualine_c = {
-        '%=', --[[ add your center compoentnts here in place of this comment ]]
+      inactive_sections = {
+        lualine_a = { 'filename' },
+        lualine_b = {},
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = { 'location' },
       },
-      lualine_x = {},
-      lualine_y = { 'filetype', 'progress' },
-      lualine_z = {
-        { 'location', separator = { right = '' }, left_padding = 2 },
-      },
-    },
-    inactive_sections = {
-      lualine_a = { 'filename' },
-      lualine_b = {},
-      lualine_c = {},
-      lualine_x = {},
-      lualine_y = {},
-      lualine_z = { 'location' },
-    },
-    tabline = {},
-    extensions = {},
-  },
+      tabline = {},
+      extensions = {},
+    }
+    vim.api.nvim_clear_autocmds {
+      event = 'ColorScheme',
+      group = 'lualine',
+    }
+    vim.api.nvim_create_autocmd('ColorScheme', {
+      callback = function()
+        require('lualine').setup()
+        vim.api.nvim_set_hl(0, 'lualine_c_normal', { ctermbg = 'NONE', force = true })
+        vim.api.nvim_set_hl(0, 'lualine_c_inactive', { ctermbg = 'NONE', force = true })
+      end,
+    })
+  end,
 }
