71a28fd18945fce7e6489a53845ab9944027c18f
Clark Farley <clark.farley@stardog.com>
2024-12-24 15:05:51 -0500

    session manager fix

--- lua/custom/plugins/session-manager.lua
+++ lua/custom/plugins/session-manager.lua
@@ -2,7 +2,7 @@ return {
   'Shatur/neovim-session-manager',
   config = function()
     local config = require 'session_manager.config'
-    local opts = { autoload_mode = { config.AutoloadMode.CurrentDir, config.AutoloadMode.LastSession } }
+    local opts = { autoload_mode = { config.AutoloadMode.CurrentDir, config.AutoloadMode.Disabled } }
     require('session_manager').setup(opts)
   end,
 }
