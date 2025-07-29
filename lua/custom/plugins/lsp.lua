32cc640615902303cb78758f549eedc65a39f540
Clark Farley <clark.farley@stardog.com>
2025-07-29 14:00:54 -0400

    add ruff lsp

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -165,6 +165,7 @@ return { -- LSP Configuration & Plugins
           },
         },
       },
+      ruff = {},
       -- pyright = {},
       -- rust_analyzer = {},
       -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
